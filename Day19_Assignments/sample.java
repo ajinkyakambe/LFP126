3f644f9d001d6a9fa9f85686153dbe46532ebe9d
ajinkya kambe <ajinkyakambe@gmail.com>
2022-04-19 00:27:11 +0530

    Comment with variables

--- Day19_Assignments/sample.java
+++ Day19_Assignments/sample.java
@@ -2,6 +2,53 @@ Hello worldpublic class sample {
     
 }
 
-comment
 
-sdffd
+PATH=C:\Python310\Scripts\
+C:\Python310\
+C:\Program Files\Common Files\Oracle\Java\javapath
+C:\Program Files (x86)\Common Files\Oracle\Java\javapath
+C:\Program Files (x86)\VMware\VMware Player\bin\
+C:\Program Files (x86)\Common Files\Intel\Shared Libraries\redist\intel64\compiler
+C:\Program Files (x86)\Intel\iCLS Client\
+C:\Program Files\Intel\iCLS Client\
+C:\WINDOWS\system32
+C:\WINDOWS
+C:\WINDOWS\System32\Wbem
+C:\WINDOWS\System32\WindowsPowerShell\v1.0\
+C:\Program Files (x86)\Intel\Intel(R) Management Engine Components\DAL
+C:\Program Files\Intel\Intel(R) Management Engine Components\DAL
+C:\Program Files (x86)\Intel\Intel(R) Management Engine Components\IPT
+C:\Program Files\Intel\Intel(R) Management Engine Components\IPT
+C:\Program Files\Intel\WiFi\bin\
+C:\Program Files\Common Files\Intel\WirelessCommon\
+C:\WINDOWS\System32\OpenSSH\
+C:\Program Files\dotnet\
+C:\Program Files\Microsoft SQL Server\130\Tools\Binn\
+C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\170\Tools\Binn\
+C:\ProgramData\chocolatey\bin
+C:\Program Files\CMake\bin
+C:\Program Files\LLVM\bin
+C:\Program Files\PuTTY\
+C:\Program Files (x86)\Yarn\bin\
+C:\TDM-GCC-64\bin
+C:\Program Files\Go\b
+C:\Program Files\Git\cmd
+C:\Program Files\Go\bin
+C:\Program Files\nodejs\
+C:\Program Files (x86)\push.bat
+C:\Users\ajinkya\AppData\Local\Microsoft\WindowsApps
+C:\Program Files\Docker Toolbox
+C:\Users\ajinkya\AppData\Local\Programs\Microsoft VS Code\bin
+C:\Exercism
+C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2019.3.1\bin
+
+C:\Users\ajinkya\AppData\Local\Yarn\bin
+C:\Users\ajinkya\AppData\Roaming\Composer\vendor\bin
+C:\Users\ajinkya\AppData\Local\GitHubDesktop\bin
+C:\Program Files\Java\jre1.8.0_321\bin
+C:\Users\ajinkya\go\bin
+C:\Users\ajinkya\AppData\Roaming\npm
+C:\cygwin64\bin
+C:\Program Files\JetBrains\IntelliJ IDEA 2022.1\bin
+
+
