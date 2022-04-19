2b4aef8b7afa251ca181622e3a224626cacccf94
ajinkya kambe <ajinkyakambe@gmail.com>
2022-04-19 13:19:58 +0530

    Starting class of program

---
+++ Day19_Assignments/Day19_Assignments/src/UserRegMain.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;  // scanner class
+
+/**
+ * Class UserRegMain with calling all the UC from 1 to 10.
+ */
+
+public class UserRegMain {
+    
+    public static void main(String args[]) {
+
+        System.out.println("Welcome to User Registration System Program");
+        System.out.println("Please select your choices");
+        Scanner input = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("1.To Validate First Name ");
+            choice = input.nextInt();
+            switch (choice) {
+                case 1:  // Uc1
+                System.out.println("===============================");
+                UC1_UserValidateFirstName.validateFirstName();
+                System.out.println("===============================");
+                    break;
+                
+                
+            }
+        }
+        while (choice != 1);
+    }
+}
+
+
+
+
+
+
